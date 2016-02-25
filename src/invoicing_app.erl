4ab14bbed69d34a9e4542a27d9963440b4c546b4
Milton Inostroza <milinostroza@groupon.com>
2016-02-25 10:10:34 -0600

    rename project

---
+++ src/invoicing_app.erl
@@ -0,0 +1,13 @@
+-module(invoicing_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2,
+	 stop/1]).
+
+start(_StartType, _StartArgs) ->
+    invoicing_sup:start_link().
+
+stop(_State) ->
+    ok.
